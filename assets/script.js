881aa4f33a800b2164ed141b1a666d3733b1f8db
TWetmore34 <tjwetmore34@gmail.com>
2022-07-02 15:21:35 -0400

    5 day forecast info displays

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+// TODO list
+// add logo display to daily forecast and 5 day
+// fix the math happening on wind speed
+
 // DOM variables
 let searchEl = document.getElementById('search')
 let submitEl = document.getElementById('submit')
@@ -7,17 +11,16 @@ let dailyEl = document.querySelectorAll('.dailyInfo')
 let imgLogo = document.createElement('img');
 let cityEl = document.getElementById('city')
 let searchDisplay = document.getElementById('display-search')
-console.log(searchDisplay.children)
+let fiveDay = document.getElementById('fiveDay')
 
-console.log(dailyEl[0])
-
-// global variables
-
-
-let urlRequest = 'https://api.openweathermap.org/data/2.5/onecall?lat=84.3880&lon=33.7490&appid=3c2f8a6a82995e1b4a0ed32b6149c563'
-let request2 = 'http://api.openweathermap.org/geo/1.0/direct?q=Atlanta&appid=3c2f8a6a82995e1b4a0ed32b6149c563'
-
+// set date variables
 dateEl.innerHTML = moment().format('(MMMM Do YYYY)')
+fiveDay.children[0].children[0].innerHTML = moment().add(1, 'days').format('l')
+fiveDay.children[1].children[0].innerHTML = moment().add(2, 'days').format('l')
+fiveDay.children[2].children[0].innerHTML = moment().add(3, 'days').format('l')
+fiveDay.children[3].children[0].innerHTML = moment().add(4, 'days').format('l')
+fiveDay.children[4].children[0].innerHTML = moment().add(5, 'days').format('l')
+
 
 // This is mroe or less the final function. When finished, it will run the url that our first function will create through geocoding and then placing lat and lon in the url
 function getLocation() {
@@ -37,41 +40,53 @@ function getLocation() {
         }).then(function(data){
             console.log(data)
             // create and append logo for daily report
-            
+            // TO DO
             let logoCode = data.current.weather[0].icon;
             var iconurl = "http://openweathermap.org/img/w/" + logoCode + ".png";
-            imgLogo.setAttribute('src', iconurl);
-            dateEl.append(imgLogo);
+            
+            logoEl.setAttribute('src', iconurl);
+            
             
             
             // display daily info
             // temp
-            let tempK = 0;
-            if(moment().format('H') < 12){
-                tempK = data.daily[0].temp.morn;
-            } else if (moment().format('H') >= 12){
-                tempK = data.daily[0].temp.day;
-            } else if (moment().format('H') > 17){
-                tempK = data.daily[0].temp.eve;
-            } else if (moment().format('H') > 20)
-            console.log(data.daily[0].temp)
-            
+            let tempK = data.current.temp;
+           
             let tempF = 1.8*(tempK-278) + 32;
             
             dailyEl[0].innerHTML = tempF.toFixed(2) + ' F';
 
             // wind
-            windSpeed = data.daily[0].wind_speed * 2.237;
+            windSpeed = data.current.wind_speed * 2.237;
             dailyEl[1].innerHTML = windSpeed.toFixed(2) + ' MPH';
 
             // humidity
-            let humidity = data.daily[0].humidity;
+            let humidity = data.current.humidity;
             dailyEl[2].innerHTML = humidity;
 
             // UVI
             let uvi = data.daily[0].uvi;
             dailyEl[3].innerHTML = uvi;
-            
+
+            // 5 day forecast
+            // temp
+            fiveDay.children[0].children[2].innerHTML = 'Temp: ' + (1.8*(data.daily[0].temp.day-278)+32).toFixed(2);
+            fiveDay.children[1].children[2].innerHTML = 'Temp: ' + (1.8*(data.daily[1].temp.day-278)+32).toFixed(2);
+            fiveDay.children[2].children[2].innerHTML = 'Temp: ' + (1.8*(data.daily[2].temp.day-278)+32).toFixed(2);
+            fiveDay.children[3].children[2].innerHTML = 'Temp: ' + (1.8*(data.daily[3].temp.day-278)+32).toFixed(2);
+            fiveDay.children[4].children[2].innerHTML = 'Temp: ' + (1.8*(data.daily[4].temp.day-278)+32).toFixed(2);
+            // Wind
+            fiveDay.children[0].children[3].innerHTML = 'Wind: ' + (data.daily[0].wind_speed * 2.237).toFixed(2) + 'MPH';
+            fiveDay.children[1].children[3].innerHTML = 'Wind: ' + (data.daily[1].wind_speed * 2.237).toFixed(2) + 'MPH';
+            fiveDay.children[2].children[3].innerHTML = 'Wind: ' + (data.daily[2].wind_speed * 2.237).toFixed(2) + 'MPH';
+            fiveDay.children[3].children[3].innerHTML = 'Wind: ' + (data.daily[3].wind_speed * 2.237).toFixed(2) + 'MPH';
+            fiveDay.children[4].children[3].innerHTML = 'Wind: ' + (data.daily[4].wind_speed * 2.237).toFixed(2) + 'MPH';
+            // Humidity
+            fiveDay.children[0].children[4].innerHTML = 'Humidity: ' + data.daily[0].humidity;
+            fiveDay.children[1].children[4].innerHTML = 'Humidity: ' + data.daily[1].humidity;
+            fiveDay.children[2].children[4].innerHTML = 'Humidity: ' + data.daily[2].humidity;
+            fiveDay.children[3].children[4].innerHTML = 'Humidity: ' + data.daily[3].humidity;
+            fiveDay.children[4].children[4].innerHTML = 'Humidity: ' + data.daily[4].humidity;
             createButton();
             handleButtonClick();
         })
